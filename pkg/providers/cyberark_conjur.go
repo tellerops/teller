bf2e8ad2f42b6fed124a92a9c1d957e341049522
Joe Garcia <joe@joe-garcia.com>
2021-08-16 16:53:23 -0400

    Add cyberark_conjur provider MVP

---
+++ pkg/providers/cyberark_conjur.go
@@ -0,0 +1,71 @@
+package providers
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/cyberark/conjur-api-go/conjurapi"
+	"github.com/cyberark/conjur-api-go/conjurapi/authn"
+	"github.com/spectralops/teller/pkg/core"
+)
+
+type ConjurClient interface {
+	RetrieveSecret(ctx context.Context, variableId string) (string, error)
+}
+
+type CyberArkConjur struct {
+	client ConjurClient
+}
+
+func NewConjurClient() (core.Provider, error) {
+	config, err := conjurapi.LoadConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	conjur, err := conjurapi.NewClientFromKey(config,
+		authn.LoginPair{
+			Login:  os.Getenv("CONJUR_AUTHN_LOGIN"),
+			APIKey: os.Getenv("CONJUR_AUTHN_API_KEY"),
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CyberArkConjur{client: conjur}, nil
+}
+
+func (c *CyberArkConjur) Name() string {
+	return "cyberark_conjur"
+}
+
+func (c *CyberArkConjur) Put(p core.KeyPath, val string) error {
+	return fmt.Errorf("%v does not implement write yet", c.Name())
+}
+func (c *CyberArkConjur) PutMapping(p core.KeyPath, m map[string]string) error {
+	return fmt.Errorf("%v does not implement write yet", c.Name())
+}
+
+func (c *CyberArkConjur) GetMapping(p core.KeyPath) ([]core.EnvEntry, error) {
+	return nil, fmt.Errorf("%v does not implement get mapping yet", c.Name())
+}
+
+func (c *CyberArkConjur) Get(p core.KeyPath) (*core.EnvEntry, error) {
+	secret, err := c.getSecret(p)
+	if err != nil {
+		return nil, err
+	}
+	if secret == "" {
+		ent := p.Missing()
+		return &ent, nil
+	}
+
+	ent := p.Found(secret)
+	return &ent, nil
+}
+
+func (c *CyberArkConjur) getSecret(kp core.KeyPath) (string, error) {
+	return c.client.RetrieveSecret(context.TODO(), kp.Path)
+}
