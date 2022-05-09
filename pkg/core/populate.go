c533bfd8a9ca5fce5e80a2ffc231490267611004
Default Profile <kaplan.elad@gmail.com>
2022-05-09 09:46:15 +0300

    fix linting
    Signed-off-by: Elad Kaplan <kaplan.elad@gmail.com>

--- pkg/core/populate.go
+++ pkg/core/populate.go
@@ -60,10 +60,10 @@ func (p *Populate) KeyPath(kp KeyPath) KeyPath {
 // parseDefaultValue returns that field name and the default value if `populateWithDefault` was found
 // Example 1: FOO:default:BAR -> the function return FOO, BAR
 // Example 2: FOO -> the function return FOO, "" (empty value)
-func (p *Populate) parseDefaultValue(evar string) (string, string) {
+func (p *Populate) parseDefaultValue(evar string) (key, defaultValue string) {
 	if strings.Contains(evar, populateWithDefault) {
 		data := strings.Split(evar, populateWithDefault)
-		if len(data) == 2 {
+		if len(data) == 2 { //nolint
 			return data[0], data[1]
 		}
 	}
