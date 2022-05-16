1a0012ab45cbf155d8aa18412ae20aa8bd764145
Elad Kaplan <kaplan.elad@gmail.com>
2022-05-16 13:32:49 +0300

    Default value for opts (#100)
    add default value for opts

--- pkg/core/populate.go
+++ pkg/core/populate.go
@@ -6,6 +6,11 @@ import (
 	"strings"
 )
 
+const (
+	populateFromEnvironment = "env:"
+	populateWithDefault     = ","
+)
+
 type Opts map[string]string
 type Populate struct {
 	opts map[string]string
@@ -21,9 +26,13 @@ func (p *Populate) FindAndReplace(path string) string {
 	populated := path
 	for k, v := range p.opts {
 		val := v
-		if strings.HasPrefix(v, "env:") {
-			evar := strings.TrimPrefix(v, "env:")
+		if strings.HasPrefix(v, populateFromEnvironment) {
+			evar := strings.TrimPrefix(v, populateFromEnvironment)
+			evar, defaultValue := p.parseDefaultValue(evar)
 			val = os.Getenv(evar)
+			if val == "" {
+				val = defaultValue
+			}
 		}
 		populated = strings.ReplaceAll(populated, fmt.Sprintf("{{%s}}", k), val)
 	}
@@ -35,11 +44,29 @@ func (p *Populate) KeyPath(kp KeyPath) KeyPath {
 	populated := path
 	for k, v := range p.opts {
 		val := v
-		if strings.HasPrefix(v, "env:") {
-			evar := strings.TrimPrefix(v, "env:")
+		if strings.HasPrefix(v, populateFromEnvironment) {
+			evar := strings.TrimPrefix(v, populateFromEnvironment)
+			evar, defaultValue := p.parseDefaultValue(evar)
 			val = os.Getenv(evar)
+			if val == "" {
+				val = defaultValue
+			}
 		}
 		populated = strings.ReplaceAll(populated, fmt.Sprintf("{{%s}}", k), val)
 	}
 	return kp.SwitchPath(path)
+}
+
+// parseDefaultValue returns that field name and the default value if `populateWithDefault` was found
+// Example 1: FOO,BAR -> the function return FOO, BAR
+// Example 2: FOO -> the function return FOO, "" (empty value)
+func (p *Populate) parseDefaultValue(evar string) (key, defaultValue string) {
+
+	if strings.Contains(evar, populateWithDefault) {
+		data := strings.SplitN(evar, populateWithDefault, 2) //nolint
+		if len(data) == 2 {                                  //nolint
+			return data[0], strings.TrimSpace(data[1])
+		}
+	}
+	return evar, ""
 }
