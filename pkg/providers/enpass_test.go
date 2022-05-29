c7b631723ab2c700e2f14168edb307f7141bfcaa
guylev008 <guy@spectralops.io>
2022-05-29 15:58:46 +0300

    wip

--- pkg/providers/enpass_test.go
+++ pkg/providers/enpass_test.go
@@ -3,8 +3,8 @@ package providers
 import (
 	"testing"
 
-	"github.com/alecthomas/assert"
 	"github.com/golang/mock/gomock"
+	"github.com/v-braun/enpass-cli/pkg/enpass"
 
 	"github.com/spectralops/teller/pkg/providers/mock_providers"
 )
@@ -13,18 +13,31 @@ func TestEnpass(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	// Assert that Bar() is invoked.
 	defer ctrl.Finish()
-	client := mock_providers.NewMockEtcdClient(ctrl)
+	client := mock_providers.NewMockEnpassClient(ctrl)
+	card := mock_providers.NewMockEnpassCard(ctrl)
 	path := "settings/prod/billing-svc"
-	pathmap := "settings/prod/billing-svc/all"
-
+	// pathmap := "settings/prod/billing-svc/all"
+	out := enpass.Card{
+		RawValue: "shazam",
+	}
+	outList := []enpass.Card{
+		out,
+	}
+	client.EXPECT().GetEntry(path, gomock.Any(), true).Return(out, nil).AnyTimes()
+	client.EXPECT().GetEntries(path, gomock.Any()).Return(outList, nil).AnyTimes()
+	card.EXPECT().Decrypt().Return("shazam", nil).AnyTimes()
+	s := Enpass{
+		client: client,
+		logger: GetTestLogger(),
+	}
 	AssertProvider(t, &s, true)
 }
 
-func TestEtcdFailures(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	// Assert that Bar() is invoked.
-	defer ctrl.Finish()
-	client := mock_providers.NewMockEtcdClient(ctrl)
+// func TestEtcdFailures(t *testing.T) {
+// 	ctrl := gomock.NewController(t)
+// 	// Assert that Bar() is invoked.
+// 	defer ctrl.Finish()
+// 	client := mock_providers.NewMockEtcdClient(ctrl)
 
-	assert.NotNil(t, err)
-}
+// 	assert.NotNil(t, err)
+// }
