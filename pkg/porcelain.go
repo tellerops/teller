27effe9291843dc6c90177fc4feb45c3c7cc7600
eladbash <elad.bash@gmail.com>
2022-07-25 22:13:16 +0300

    .

--- pkg/porcelain.go
+++ pkg/porcelain.go
@@ -29,7 +29,6 @@ func (p *Porcelain) StartWizard() (*core.WizardAnswers, error) {
 
 	providers := BuiltinProviders{}
 	providerNames := providers.ProviderHumanToMachine()
-	fmt.Println("Available providers:")
 	displayProviders := lo.Keys(providerNames)
 	sort.Strings(displayProviders)
 	// the questions to ask
