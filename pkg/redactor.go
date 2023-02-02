d093345ed674a55f1df730d8e9f8cdea411f82c4
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-02-01 21:56:06 -0600

    Fix no redacted output until process ends
    This issue happens because `Teller.execCmd` calls `CombinedOutput`
    which keeps outputs on RAM until the child process ends.
    
    This commit rewrites `Redactor` so that it works as an adapter of
    `io.Writable`. This implementation writes output immediately to stdout
    and stderr.

--- pkg/redactor.go
+++ pkg/redactor.go
@@ -1,6 +1,9 @@
 package pkg
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"sort"
 	"strings"
 
@@ -8,24 +11,45 @@ import (
 )
 
 type Redactor struct {
-	Entries []core.EnvEntry
+	io.WriteCloser
+	err <-chan error
 }
 
-func NewRedactor(entries []core.EnvEntry) *Redactor {
-	return &Redactor{
-		Entries: entries,
-	}
-}
-
-func (r *Redactor) Redact(s string) string {
-	redacted := s
-	entries := append([]core.EnvEntry(nil), r.Entries...)
-
+func NewRedactor(dist io.Writer, entries []core.EnvEntry) *Redactor {
+	entries = append([]core.EnvEntry(nil), entries...)
 	sort.Sort(core.EntriesByValueSize(entries))
-	for i := range entries {
-		ent := entries[i]
-		redacted = strings.ReplaceAll(redacted, ent.Value, ent.RedactWith)
-	}
 
-	return redacted
+	r, w := io.Pipe()
+	ch := make(chan error)
+	go func() {
+		defer close(ch)
+
+		s := bufio.NewScanner(r)
+		buf := make([]byte, 0, 64*1024)
+		s.Buffer(buf, 10*1024*1024) // 10MB lines correlating to 10MB files max (bundles?)
+
+		for s.Scan() {
+			line := s.Text()
+			for _, ent := range entries {
+				line = strings.ReplaceAll(line, ent.Value, ent.RedactWith)
+			}
+			if _, err := fmt.Fprintln(dist, line); err != nil {
+				ch <- r.CloseWithError(err)
+				return
+			}
+		}
+		ch <- s.Err()
+	}()
+
+	return &Redactor{
+		WriteCloser: w,
+		err:         ch,
+	}
+}
+
+func (r *Redactor) Close() error {
+	err := r.WriteCloser.Close()
+	<-r.err
+
+	return err
 }
--- pkg/redactor_test.go
+++ pkg/redactor_test.go
@@ -1,6 +1,8 @@
 package pkg
 
 import (
+	"bytes"
+	"io"
 	"testing"
 
 	"github.com/alecthomas/assert"
@@ -8,12 +10,10 @@ import (
 )
 
 func TestRedactorOverlap(t *testing.T) {
-
 	// in this case we dont want '123' to appear in the clear after all redactions are made.
 	// it can happen if the smaller secret get replaced first because both
 	// secrets overlap. we need to ensure the wider secrets always get
 	// replaced first.
-
 	entries := []core.EnvEntry{
 		{
 			ProviderName: "test",
@@ -30,26 +30,33 @@ func TestRedactorOverlap(t *testing.T) {
 			RedactWith:   "**SOME_KEY**",
 		},
 	}
-	redactor := Redactor{Entries: entries}
 	s := `
-	func Foobar(){
-		secret := "hello"
-		callService(secret, "hello123")
-		// hello, hello123
-	}
-	`
+func Foobar(){
+	secret := "hello"
+	callService(secret, "hello123")
+	// hello, hello123
+}
+`
 	sr := `
-	func Foobar(){
-		secret := "**OTHER_KEY**"
-		callService(secret, "**SOME_KEY**")
-		// **OTHER_KEY**, **SOME_KEY**
-	}
-	`
+func Foobar(){
+	secret := "**OTHER_KEY**"
+	callService(secret, "**SOME_KEY**")
+	// **OTHER_KEY**, **SOME_KEY**
+}
+`
 
-	assert.Equal(t, redactor.Redact(s), sr)
+	buf := bytes.NewBuffer(nil)
+	redactor := NewRedactor(buf, entries)
+
+	_, err := io.WriteString(redactor, s)
+	assert.NoError(t, err)
+
+	err = redactor.Close()
+	assert.NoError(t, err)
+
+	assert.Equal(t, buf.String(), sr)
 }
 func TestRedactorMultiple(t *testing.T) {
-
 	entries := []core.EnvEntry{
 		{
 			ProviderName: "test",
@@ -66,25 +73,32 @@ func TestRedactorMultiple(t *testing.T) {
 			RedactWith:   "**OTHER_KEY**",
 		},
 	}
-	redactor := Redactor{Entries: entries}
 	s := `
-	func Foobar(){
-		secret := "loot"
-		callService(secret, "shazam")
-	}
-	`
+func Foobar(){
+	secret := "loot"
+	callService(secret, "shazam")
+}
+`
 	sr := `
-	func Foobar(){
-		secret := "**OTHER_KEY**"
-		callService(secret, "**SOME_KEY**")
-	}
-	`
+func Foobar(){
+	secret := "**OTHER_KEY**"
+	callService(secret, "**SOME_KEY**")
+}
+`
 
-	assert.Equal(t, redactor.Redact(s), sr)
+	buf := bytes.NewBuffer(nil)
+	redactor := NewRedactor(buf, entries)
+
+	_, err := io.WriteString(redactor, s)
+	assert.NoError(t, err)
+
+	err = redactor.Close()
+	assert.NoError(t, err)
+
+	assert.Equal(t, buf.String(), sr)
 }
 
 func TestRedactor(t *testing.T) {
-
 	entries := []core.EnvEntry{
 		{
 			ProviderName: "test",
@@ -94,19 +108,36 @@ func TestRedactor(t *testing.T) {
 			RedactWith:   "**NOPE**",
 		},
 	}
-	redactor := Redactor{Entries: entries}
 	s := `
-	func Foobar(){
-		secret := "shazam"
-		callService(secret, "shazam")
-	}
-	`
+func Foobar(){
+	secret := "shazam"
+	callService(secret, "shazam")
+}
+`
 	sr := `
-	func Foobar(){
-		secret := "**NOPE**"
-		callService(secret, "**NOPE**")
-	}
-	`
+func Foobar(){
+	secret := "**NOPE**"
+	callService(secret, "**NOPE**")
+}
+`
 
-	assert.Equal(t, redactor.Redact(s), sr)
+	buf := bytes.NewBuffer(nil)
+	redactor := NewRedactor(buf, entries)
+
+	_, err := io.WriteString(redactor, s)
+	assert.NoError(t, err)
+
+	err = redactor.Close()
+	assert.NoError(t, err)
+
+	assert.Equal(t, buf.String(), sr)
+}
+
+func TestRedactor_Close(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+	redactor := NewRedactor(buf, nil)
+
+	assert.NoError(t, redactor.Close())
+	// can be close more than once.
+	assert.NoError(t, redactor.Close())
 }
